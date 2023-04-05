97a0a20984a262ed620d97b23b27a938f2a0627e
IvanaMENDJEMEN <soniayoumbi03@gmail.com>
2023-04-05 12:43:36 +0100

    Ajout de l'exercice 2

--- TP2_Java/src/HelloUniverse.java
+++ TP2_Java/src/HelloUniverse.java
@@ -1,3 +1,14 @@
 public class HelloUniverse {
-
+    public static void main(String[] args) {
+        int x = 9;
+        int annee = 2003;
+        String phrase ="En %d les planètes du système solaire étaient au nombre de : %d ";
+        if(annee < 2006){
+            System.out.printf(phrase,annee,x);
+        }
+        else{
+            x--;
+            System.out.printf(phrase,annee,x);
+        }
+    }
 }
